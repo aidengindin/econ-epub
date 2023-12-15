acf79f65dfeef0146ddc0f61de5f061a884784c1
Aiden Gindin <aiden@thegindins.com>
2023-12-15 13:35:37 -0500

    generate full issue markdown

--- econ-epub.go
+++ econ-epub.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"github.com/PuerkitoBio/goquery"
-	"io"
 	"log"
 	"net/http"
+	"strings"
 	"time"
+	"fmt"
 )
 
 const baseURL = "https://www.economist.com"
@@ -16,39 +17,40 @@ type Section struct {
 	urls []string
 }
 
-func main() {
-	body := makeRequest()
-
-	doc, err := goquery.NewDocumentFromReader(body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// headline := headline(*doc)
-	// worldThisWeek := worldThisWeek(*doc)
-	// leaders := leaders(*doc)
-	// sections := sections(*doc)
+type Article struct {
+	supertitle string
+	title string
+	subtitle string
+	body string
 }
 
-func makeRequest() io.Reader {
+func main() {
+	doc := getDocument(weeklyEditionURL)
+
+	markdown := buildMarkdown(doc)
+	fmt.Println(markdown)
+}
+
+func getDocument(url string) goquery.Document {
 	client := &http.Client{
 		Timeout: 30 * time.Second,
 	}
 
-	response, err := client.Get(weeklyEditionURL)
+	response, err := client.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// defer response.Body.Close()
-	return response.Body
+
+	// return response.Body
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return *doc
 }
 
 func headline(doc goquery.Document) string {
-	var headline string
-	doc.Find(".weekly-edition-header__headline").Each(func(i int, s *goquery.Selection) {
-		headline = s.Text()
-	})
-	return headline
+	return doc.Find(".weekly-edition-header__headline").Text()
 }
 
 func worldThisWeek(doc goquery.Document) []string {
@@ -78,5 +80,72 @@ func sections(doc goquery.Document) []Section {
 		sections = append(sections, Section{title, urls})
 	})
 	return sections
+}
+
+func getArticle(url string) Article {
+	doc := getDocument(url)
+
+	bodyBuilder := strings.Builder{}
+
+	supertitle := doc.Find(".css-1mdqtqm.e11bnqe00").Text()
+	title := doc.Find(".css-6p5r16.e11fb5bd0").Text()
+	subtitle := doc.Find(".css-qm1vln.elmr55g0").Text()
+
+	doc.Find(".css-1qkdneh.ee5d8yd2").Children().Filter(":not(style)").Each(func(_ int, s *goquery.Selection) {
+		// I hate pseudoselectors
+		if isRealContent(s) {
+			if s.Is("h2") {
+				bodyBuilder.WriteString("#### " + s.Text() + "\n\n")
+			} else if s.Is("p") {
+				bodyBuilder.WriteString(s.Text() + "\n\n")
+			} else if s.Is("div") && (s.HasClass("css-0") || s.HasClass("css-1bzwzkr")) {
+				imageURL := s.Find("img").AttrOr("src", "")
+				imageCaption := s.Find(".css-6pzdis.edweubf1").Text()
+				bodyBuilder.WriteString("![" + imageCaption + "](" + imageURL + ")\n\n")
+			}
+		}
+	})
+
+	return Article{supertitle, title, subtitle, bodyBuilder.String()}
+}
+
+func isRealContent(s *goquery.Selection) bool {
+	return !s.HasClass("css-1lm38nn") && !s.HasClass("adComponent_advert__V79Pp")
+}
+
+func buildMarkdown(doc goquery.Document) string {
+	headline := headline(doc)
+	worldThisWeek := worldThisWeek(doc)
+	leaders := leaders(doc)
+	sections := sections(doc)
+
+	builder := strings.Builder{}
+
+	builder.WriteString("---\ntitle: \"" + headline + "\"\n---\n\n")
+
+	builder.WriteString("# The world this week\n\n")
+	for _, url := range worldThisWeek {
+		article := getArticle(url)
+		builder.WriteString("## " + article.title + "\n\n")
+		builder.WriteString(article.body)
+	}
+
+	builder.WriteString("# Leaders\n\n")
+	for _, url := range leaders {
+		article := getArticle(url)
+		builder.WriteString("## " + article.title + "\n\n")
+		builder.WriteString(article.body)
+	}
+
+	for _, section := range sections {
+		builder.WriteString("# " + section.title + "\n\n")
+		for _, url := range section.urls {
+			article := getArticle(url)
+			builder.WriteString("## " + article.title + "\n\n")
+			builder.WriteString(article.body)
+		}
+	}
+
+	return builder.String()
 }
 
